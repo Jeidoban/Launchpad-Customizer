cdecf2d02cb78df7a1335e4c850d3354e294d04b
Jade Westover <jade424433@gmail.com>
2022-07-27 16:58:54 -0700

    Initial Commit

---
+++ Launchpad Customizer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Launchpad Customizer
+//
+//  Created by Jade Westover on 7/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Launchpad Customizer/Launchpad_CustomizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Launchpad_CustomizerApp.swift
+//  Launchpad Customizer
+//
+//  Created by Jade Westover on 7/27/22.
+//
+
+import SwiftUI
+
+@main
+struct Launchpad_CustomizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
